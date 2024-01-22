1419a07844c27e39c1b0da24fa5d7da0fdb24246
hmater <hmater@gmail.com>
2024-01-22 03:53:16 -0300

    asd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 import sys
+import tempfile
+  
 from google.cloud import storage
 
 
@@ -27,17 +29,43 @@ def extract_frames(input_file, output_folder):
     video_capture.release()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py input_file_path")
+    #if len(sys.argv) != 2:
+        #print("Usage: python main.py input_file_path")
         #sys.exit(1)
 
-    input_file_path = sys.argv[1]
+    #input_file_path = sys.argv[1]
     output_folder = "output"
 
     client = storage.Client()
     for blob in client.list_blobs('ap_test_collection_aw1_raw_input', prefix='1000000070d5eb51/2024/01/17/14'):
         if(str(blob.name).endswith('.h265')):
             print (str(blob.name))
+            video_gs = blob
+
+
+    temp = tempfile.NamedTemporaryFile()
+    
+    try:
+        temp.write(video_gs)
+        output_folder = "output"
+        extract_frames(temp, output_folder)
+    finally:
+        temp.close()
+
+
+    #file_name = blob.name
+    #_, temp_local_filename = tempfile.mkstemp()
+
+    # Download file from bucket.
+    #blob.download_to_filename(temp_local_filename)
+
+
+
+
+    #input_file_path='gs://'+video_gs
+    
+
+
 
     #extract_frames(input_file_path, output_folder)
 
