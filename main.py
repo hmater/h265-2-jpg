249c80e92ff809d2af738886b54a16a003a7a6ff
hmater <hmater@gmail.com>
2024-01-22 05:43:36 -0300

    *st

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from google.cloud import bigquery
 def save(file_name):
     print("saving name to db...")
     #dt = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    dt = datetime.now()
+    dt = str(datetime.now())
     client = bigquery.Client()
     table_id = "awentia-data-pipeline.DataVision.processed_files"
     job_config = bigquery.QueryJobConfig(destination=table_id)
